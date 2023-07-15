9c3f60b4a4a5f10485debde77f38db41327eb127
JoileJr <joilejunior2@gmail.com>
2023-07-15 18:20:39 -0300

    ajustes

--- src/pages/sign-up/SingUp.jsx
+++ src/pages/sign-up/SingUp.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 
 import styles from './SingUp.module.css'
 
@@ -7,39 +8,78 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 
-const cor_racas = [
-  {
-    value: 'Branco',
-  },
-  {
-    value: 'Preto',
-  },
-  {
-    value: 'Amarela',
-  },
-  {
-    value: 'Indigena',
-  },
-  {
-    value: 'Prefiro não informar'
-  }
-];
 
-const sexo = [
+
+const roles = [
   {
-    value: 'Masculino',
+    name: 'Estudante',
+    value: 1,
   },
   {
-    value: 'Feminino',
-  }
+    name: 'Professor(a)',
+    value: 2,
+  },
 ];
 
 function SingUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [datebirth, setDatebirth] = useState('');
+  const [cpf, setCPF] = useState('');
+  const [role, setRole] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  function handleCpfChange(e) {
+    let cpfDigitado = e.target.value;
+    cpfDigitado = cpfDigitado.replace(/\D/g, '');
+    cpfDigitado = cpfDigitado.substring(0, 11);
+    cpfDigitado = cpfDigitado.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+    setCPF(cpfDigitado);
+  }
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleDateBirthChange = (e) => {
+    setDatebirth(e.target.value);
+  };
+
+  const handleRoleChange = (e) => {
+    setRole(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const person = {
+      name: name,
+      email: email,
+      cpf: cpf,
+      birthDate: datebirth,
+      role: role,
+      password: password
+    }
+    console.log(person)
+  }
   return (
     <div className={styles.container}>
       <Card className={styles.card}>
         <h1>Cadastre</h1>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
           <div className={styles.row}>
             <TextField 
               id="name" 
@@ -48,6 +88,8 @@ function SingUp() {
               variant="outlined" 
               color="warning"
               className={styles.input} 
+              value={name}
+              onChange={handleNameChange}
             />
             <TextField 
               id="email" 
@@ -56,35 +98,11 @@ function SingUp() {
               variant="outlined" 
               color="warning"
               className={styles.input} 
+              value={email}
+              onChange={handleEmailChange}
             />
           </div>
           <div className={styles.row}>
-            <TextField 
-              id="rg" 
-              type="text" 
-              label="RG" 
-              variant="outlined" 
-              color="warning"
-              className={styles.input} 
-            />
-            <TextField 
-              id="cpf" 
-              type="text" 
-              label="CPF" 
-              variant="outlined" 
-              color="warning"
-              className={styles.input} 
-            />   
-          </div>
-          <div className={styles.row}>
-            <TextField 
-              id="orgao-emissor" 
-              type="text" 
-              label="Orgão emissor" 
-              variant="outlined" 
-              color="warning"
-              className={styles.input} 
-            />
             <div className={styles.dataNascimento}>
               <label>Data de Nascimento</label>
               <TextField 
@@ -92,37 +110,65 @@ function SingUp() {
                 type="date" 
                 variant="outlined" 
                 color="warning"
+                value={datebirth}
+                onChange={handleDateBirthChange}
               /> 
-            </div>              
+            </div> 
+            <TextField 
+              id="cpf" 
+              type="text" 
+              label="CPF" 
+              variant="outlined" 
+              color="warning"
+              className={styles.input} 
+              value={cpf}
+              onChange={handleCpfChange}
+            />   
           </div>
           <div className={styles.row}>
-            <TextField
-              id="outlined-select-currency"
-              select
-              label="Cor/Raça"
-              color="warning"
-              className={styles.input}
-            >
-              {cor_racas.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.value}
-                </MenuItem>
-              ))}
-            </TextField> 
-            <TextField
-              id="outlined-select-currency"
-              select
-              label="Sexo"
-              color="warning"
-              className={styles.input}
-            >
-              {sexo.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.value}
-                </MenuItem>
-              ))}
-            </TextField> 
+            <div className={styles.role}>
+              <label>Cadastrar-se como</label>
+              <TextField
+                id="role"
+                select
+                color="warning"
+                placeholder='Cadastrar-se como'
+                className={styles.input}
+                value={role}
+                onChange={handleRoleChange}
+              >
+                {roles.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.name}
+                  </MenuItem>
+                ))}
+              </TextField> 
+            </div>
+            
           </div>
+          <div className={styles.row}>
+            <TextField 
+              id="password" 
+              type="password" 
+              label="Senha" 
+              variant="outlined" 
+              color="warning"
+              className={styles.input} 
+              value={password}
+              onChange={handlePasswordChange}
+            />
+            <TextField 
+              id="confirm_password" 
+              type="password" 
+              label="Confirme sua senha" 
+              variant="outlined" 
+              color="warning"
+              className={styles.input} 
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
+            />
+          </div>
+          <Button type="submit" variant="contained" className={styles.button} color="warning">Entrar</Button>
         </form>
       </Card>
     </div>
