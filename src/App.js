a8c155c790077aa794e8029afc4917f85e37eef4
JoileJr <joilejunior2@gmail.com>
2023-07-18 10:32:13 -0300

    começando home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login/Login";
 import SingUp from "./pages/sign-up/SingUp";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes >
         <Route exact path="/" element={ <Login/> }/> 
         <Route exact path="/sign-up" element={ <SingUp/> }/> 
+        <Route exact path="/home" element={ <Home/> }/> 
       </Routes>
     </Router>
   );
